7d59ccdb486b0e3948f67a73312c13fb7c5bb6ae
valisoa01 <valisoatolotriniaina@gmail.com>
2026-05-26 13:54:40 +0300

    add ft_strdup

--- rendu/ft_strdup/ft_strdup.c
+++ rendu/ft_strdup/ft_strdup.c
@@ -1,25 +1,27 @@
 #include<stddef.h>
 int ft_strlen(char *str)
 {
-    int len;
-
-    len = 0;
-    while(str[len] != '\0')
-        len++;
-    return(len);
-}
-char *strdup(char *str)
-{
-    int len;
-    char *dup;
     int i;
 
     i = 0;
-    len =  ft_strlen(str);
-    dup = (char *)malloc((len + 1));
+    while(str[i]!= '\0')
+    {
+        i++;
+    }
+    return(i);
+}
+char ft_strdup(char *str)
+{
+    char *dup;
+    int i;
+    int len;
+
+    i = 0;
+    len = ft_strlen(str);
+    dup = (char *)malloc(len + 1);
     if(!dup)
         return(NULL);
-    while(str[i] != '\0');
+    while(str[i] != '\0')
     {
         dup[i] = str[i];
         i++;
